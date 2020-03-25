aab0068ca2217333336171c93e9e2c1852b17ac6
Aaron Barchet <aaron.barchet@gmail.com>
2020-03-25 13:59:15 -0400

    Ran program and registered a user

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        [Display(Name = "Note Id")]
+        public int NoteId { get; set; }
+
+        [Required]
+        [Display(Name = "Owner Id")]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "There are too many characters in this field.")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
