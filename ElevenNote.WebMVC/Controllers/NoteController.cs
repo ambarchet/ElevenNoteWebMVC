eea6475921b25fb6f3d1580c76e9201d5bc1e586
Aaron Barchet <aaron.barchet@gmail.com>
2020-03-25 15:27:24 -0400

    Added Index and Create Note Views

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,39 @@
+﻿using ElevenNote.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // NOTE: GET
+        public ActionResult Index()
+        {
+            //initializing a new instance of the NoteListItem as an IEnumerable with the [0] syntax. //
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        // CREATE NOTE: GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE NOTE: POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
